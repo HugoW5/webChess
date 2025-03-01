24313ac5ee9c53e865627e45a0ac30995cbe9706
HugoW5 <hugo05.wakt@gmail.com>
2025-03-01 10:52:49 +0100

    getValidMoves() function

--- chess.js
+++ chess.js
@@ -1,6 +1,7 @@
 let cvs = document.getElementById("cvs");
 let ctx = cvs.getContext("2d");
 let boardSize = 800;
+let selectedPiece = {};
 
 renderBoard();
 setTimeout(() => {
@@ -8,22 +9,55 @@ setTimeout(() => {
 }, 100);
 
 cvs.addEventListener("mousedown", (e) => {
+  ctx.clearRect(0, 0, boardSize, boardSize);
   let pos = getSquare(e.offsetX, e.offsetY);
-  let middleSquarePos = {
-    x: pos.x * (boardSize / 8) + boardSize / 8 / 2,
-    y: pos.y * (boardSize / 8) + boardSize / 8 / 2,
-  };
-  ctx.beginPath();
-  ctx.fillStyle = "#4fc215";
-  ctx.arc(
-    middleSquarePos.x,
-    middleSquarePos.y,
-    boardSize / 8 / 4,
-    0,
-    2 * Math.PI
+  renderBoard();
+  ctx.fillStyle = "#eb4034";
+  ctx.fillRect(
+    pos.x * (boardSize / 8),
+    pos.y * (boardSize / 8),
+    boardSize / 8,
+    boardSize / 8
   );
-  ctx.fill();
+  renderPieces();
+
+  let tmpPiece = pieces.find(({ x, y }) => {
+    return x === pos.x && y === pos.y;
+  });
+  if (tmpPiece != undefined) {
+    if (pos == tmpPiece.getValidMoves()) {
+      
+    } else {
+      interactWithPiece(tmpPiece);
+    }
+  }
 });
+
+/**
+ * @param {Piece} piece
+ */
+function interactWithPiece(piece) {
+  console.log(piece);
+  selectedPiece = piece;
+  let moves = piece.getValidMoves();
+  //show valid moves
+  moves.forEach((move) => {
+    let middleSquarePos = {
+      x: move.x * (boardSize / 8) + boardSize / 8 / 2,
+      y: move.y * (boardSize / 8) + boardSize / 8 / 2,
+    };
+    ctx.beginPath();
+    ctx.fillStyle = "#4fc215";
+    ctx.arc(
+      middleSquarePos.x,
+      middleSquarePos.y,
+      boardSize / 8 / 4,
+      0,
+      2 * Math.PI
+    );
+    ctx.fill();
+  });
+}
 
 function renderBoard() {
   for (let i = 0; i < 8; i++) {
@@ -36,7 +70,6 @@ function renderBoard() {
       } else {
         ctx.fillStyle = "#6e6e6e";
       }
-
       ctx.fillRect(x, y, boardSize / 8, boardSize / 8);
     }
   }
@@ -56,7 +89,6 @@ function getSquare(x1, y1) {
 
 function renderPieces() {
   pieces.forEach((piece) => {
-    console.log(piece);
     ctx.drawImage(
       piece.image,
       piece.x * (boardSize / 8),
--- piece.js
+++ piece.js
@@ -1,13 +1,5 @@
 let pieces = [];
-
 class Piece {
-  /**
-   * @param {number} x Piece row position
-   * @param {number} y Piece col position
-   * @param {string} name Piece name
-   * @param {HTMLImageElement} image Piece SVG Image Element
-   * @param {string} color Piece color
-   */
   constructor(x, y, name, image, color) {
     this.name = name;
     this.color = color;
@@ -15,7 +7,208 @@ class Piece {
     this.x = x;
     this.y = y;
   }
+
+  /**
+   * Returns an array of valid moves for the current piece.
+   * @returns {Array<{x: number, y: number}>}
+   */
+  getValidMoves() {
+    let validMoves = [];
+
+    if (this.name === "Pawn") {
+      this.#getPawnMoves(validMoves);
+    } else if (this.name === "Rook") {
+      this.#getRookMoves(validMoves);
+    } else if (this.name === "Knight") {
+      this.#getKnightMoves(validMoves);
+    } else if (this.name === "Bishop") {
+      this.#getBishopMoves(validMoves);
+    } else if (this.name === "Queen") {
+      this.#getQueenMoves(validMoves);
+    } else if (this.name === "King") {
+      this.#getKingMoves(validMoves);
+    }
+
+    return validMoves;
+  }
+
+  /**
+   * @param {Array} validMoves
+   */
+  #getPawnMoves(validMoves) {
+    const direction = this.color === "white" ? -1 : 1;
+
+    if (!this.isOccupied(this.x, this.y + direction)) {
+      validMoves.push({ x: this.x, y: this.y + direction });
+
+      if (
+        (this.color === "white" && this.y === 6) ||
+        (this.color === "black" && this.y === 1)
+      ) {
+        if (!this.isOccupied(this.x, this.y + direction * 2)) {
+          validMoves.push({ x: this.x, y: this.y + direction * 2 });
+        }
+      }
+    }
+
+    if (this.isEnemyPiece(this.x - 1, this.y + direction)) {
+      validMoves.push({ x: this.x - 1, y: this.y + direction });
+    }
+    if (this.isEnemyPiece(this.x + 1, this.y + direction)) {
+      validMoves.push({ x: this.x + 1, y: this.y + direction });
+    }
+  }
+
+  /**
+   * @param {Array} validMoves
+   */
+  #getRookMoves(validMoves) {
+    const directions = [
+      { x: 0, y: 1 },
+      { x: 0, y: -1 },
+      { x: 1, y: 0 },
+      { x: -1, y: 0 },
+    ];
+
+    for (let direction of directions) {
+      let x = this.x;
+      let y = this.y;
+
+      while (true) {
+        x += direction.x;
+        y += direction.y;
+
+        if (x < 0 || x > 7 || y < 0 || y > 7) break;
+
+        if (this.isOccupied(x, y)) {
+          if (this.isEnemyPiece(x, y)) {
+            validMoves.push({ x, y });
+          }
+          break;
+        } else {
+          validMoves.push({ x, y });
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {Array} validMoves
+   */
+  #getKnightMoves(validMoves) {
+    const knightMoves = [
+      { x: 2, y: 1 },
+      { x: 2, y: -1 },
+      { x: -2, y: 1 },
+      { x: -2, y: -1 },
+      { x: 1, y: 2 },
+      { x: 1, y: -2 },
+      { x: -1, y: 2 },
+      { x: -1, y: -2 },
+    ];
+
+    for (let move of knightMoves) {
+      let newX = this.x + move.x;
+      let newY = this.y + move.y;
+
+      if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
+        if (!this.isOccupied(newX, newY) || this.isEnemyPiece(newX, newY)) {
+          validMoves.push({ x: newX, y: newY });
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {Array} validMoves
+   */
+  #getBishopMoves(validMoves) {
+    // Diagonal moves
+    const directions = [
+      { x: 1, y: 1 },
+      { x: 1, y: -1 },
+      { x: -1, y: 1 },
+      { x: -1, y: -1 }, // Up-right, Up-left, Down-right, Down-left
+    ];
+
+    for (let direction of directions) {
+      let x = this.x;
+      let y = this.y;
+
+      while (true) {
+        x += direction.x;
+        y += direction.y;
+
+        if (x < 0 || x > 7 || y < 0 || y > 7) break;
+
+        if (this.isOccupied(x, y)) {
+          if (this.isEnemyPiece(x, y)) {
+            validMoves.push({ x, y });
+          }
+          break;
+        } else {
+          validMoves.push({ x, y });
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {Array} validMoves
+   */
+  #getQueenMoves(validMoves) {
+    this.#getRookMoves(validMoves);
+    this.#getBishopMoves(validMoves);
+  }
+
+  /**
+   * @param {Array} validMoves
+   */
+  #getKingMoves(validMoves) {
+    const kingMoves = [
+      { x: 1, y: 0 },
+      { x: -1, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: -1 }, // Horizontal and vertical
+      { x: 1, y: 1 },
+      { x: 1, y: -1 },
+      { x: -1, y: 1 },
+      { x: -1, y: -1 }, // diagonal
+    ];
+
+    for (let move of kingMoves) {
+      let newX = this.x + move.x;
+      let newY = this.y + move.y;
+
+      if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
+        if (!this.isOccupied(newX, newY) || this.isEnemyPiece(newX, newY)) {
+          validMoves.push({ x: newX, y: newY });
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {number} x The x-coordinate to check.
+   * @param {number} y The y-coordinate to check.
+   * @returns {boolean} True if the position is occupied.
+   */
+  isOccupied(x, y) {
+    return pieces.some((piece) => piece.x === x && piece.y === y);
+  }
+
+  /**
+   * @param {number} x The x-coordinate to check.
+   * @param {number} y The y-coordinate to check.
+   * @returns {boolean} true if the position is occupied by an enemy piece.
+   */
+  isEnemyPiece(x, y) {
+    return pieces.some(
+      (piece) => piece.x === x && piece.y === y && piece.color !== this.color
+    );
+  }
 }
+
 // #region Add pieces
 let whitePawnImage = new Image();
 whitePawnImage.src = "./pieces/pawn-w.svg";
@@ -40,12 +233,12 @@ pieces.push(new Piece(7, 0, "Rook", blackRookImage, "black"));
 let whiteKnightImage = new Image();
 whiteKnightImage.src = "./pieces/knight-w.svg";
 pieces.push(new Piece(1, 7, "Knight", whiteKnightImage, "white"));
-pieces.push(new Piece(6, 7, "Kngiht", whiteKnightImage, "white"));
+pieces.push(new Piece(6, 7, "Knight", whiteKnightImage, "white"));
 
 let blackKnightImage = new Image();
 blackKnightImage.src = "./pieces/knight-b.svg";
 pieces.push(new Piece(1, 0, "Knight", blackKnightImage, "black"));
-pieces.push(new Piece(6, 0, "Kngiht", blackKnightImage, "black"));
+pieces.push(new Piece(6, 0, "Knight", blackKnightImage, "black"));
 
 let whiteBishopImage = new Image();
 whiteBishopImage.src = "./pieces/bishop-w.svg";
